066945b9ec00977d0253ac14d81b2c6a4eeb7f39
soriasayer <soria.sayer@signavio.com>
2021-11-26 12:01:44 +0100

    working on three requirement

--- greeting.js
+++ greeting.js
@@ -1,1 +1,10 @@
-console.log('asdf')
+const greet = (name) => {
+
+    if(name === 'Bob'){
+        return `Hello, ${name}`
+    } else if (name === null){
+        return 'Hello, my friend'
+    } else if (name == name.toUpperCase()){
+        return `HELLO ${name}!`
+    }
+}
