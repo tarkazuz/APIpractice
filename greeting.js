77b505cd9f4729bca203a0a92e230c2a814c24ae
soriasayer <soria.sayer@signavio.com>
2021-11-26 18:21:00 +0100

    refactor

--- greeting.js
+++ greeting.js
@@ -1,20 +1,26 @@
 const greet = (name) => {
-    let nameResult 
-    if(typeof(name) === 'string' || !name ){
+
+    const isArray = Array.isArray(name)
+    const isString = typeof(name) === 'string'
+
+    if(isString || !name ){
         if(name === 'Bob'){
-            nameResult = `Hello, ${name}`
+            return `Hello, ${name}`
         } else if (!name){
-            nameResult = 'Hello, my friend'
-        } else if (name === name?.toUpperCase()){
-            nameResult = `HELLO ${name}!`
-        }
-    } else if(name.length === 2) {
-        nameResult = `Hello, ${name[0]} and ${name[1]}`
-    } else if(name.length === 3) {
-        nameResult = `Hello, ${name[0]}, ${name[1]} and ${name[2]}` 
+            return 'Hello, my friend'
+        } else if (name === name.toUpperCase()){
+            return `HELLO ${name}!`
+        } else return `${name} is invalid!`
     }
-    console.log(nameResult)
-    return nameResult
+
+    if(isArray) {
+        const arrLength = name.length
+        if(arrLength === 2){
+            return `Hello, ${name[0]} and ${name[1]}`
+        }else if(arrLength === 3){
+            return `Hello, ${name[0]}, ${name[1]} and ${name[2]}` 
+        } else return `${name} is invalid!`
+    }  
 }
 
 greet(['s', 'c', 'r'])
