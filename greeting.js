ebad542d8ba5a907c3ab86b62a7869fb37786447
soriasayer <soria.sayer@signavio.com>
2021-11-26 14:09:58 +0100

    more logic added

--- greeting.js
+++ greeting.js
@@ -1,14 +1,22 @@
 const greet = (name) => {
-    let nameResualt 
-    if(name === 'Bob'){
-        nameResualt = `Hello, ${name}`
-    } else if (!name){
-        nameResualt = 'Hello, my friend'
-    } else if (name === name?.toUpperCase()){
-        nameResualt = `HELLO ${name}!`
+    let nameResult 
+    if(typeof(name) === 'string' || !name ){
+        if(name === 'Bob'){
+            nameResult = `Hello, ${name}`
+        } else if (!name){
+            nameResult = 'Hello, my friend'
+        } else if (name === name?.toUpperCase()){
+            nameResult = `HELLO ${name}!`
+        }
+    } else if(name.length === 2) {
+        nameResult = `Hello, ${name[0]} and ${name[1]}`
+    } else if(name.length === 3) {
+        nameResult = `Hello, ${name[0]}, ${name[1]} and ${name[2]}` 
     }
-    console.log(nameResualt)
-    return nameResualt
+    console.log(nameResult)
+    return nameResult
 }
+
+greet(['s', 'c', 'r'])
 
 module.exports = greet
--- greeting.test.js
+++ greeting.test.js
@@ -7,8 +7,14 @@ describe('Greet', ()=> {
     it('should return correctly with parameter Null', () => {
         expect(greet(null)).toBe('Hello, my friend')
     })
-    it('should return correctly with parameter BOB', () => {
-        expect(greet('BOB')).toBe('HELLO BOB!')
+    it('should return correctly with parameter CECILE', () => {
+        expect(greet('CECILE')).toBe('HELLO CECILE!')
+    })
+    it('should return correctly with parameter Array of length two', () => {
+        expect(greet(['Soria', 'Cecile'])).toBe('Hello, Soria and Cecile')
+    })
+    it('should return correctly with parameter Array of length three', () => {
+        expect(greet(['Soria', 'Cecile', 'Book'])).toBe('Hello, Soria, Cecile and Book')
     })
 
 })
