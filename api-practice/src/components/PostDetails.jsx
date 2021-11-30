7b1b7c127cca5ebf8470f8d748b4bdaea3c189b7
Zuzanna Tarka <zuzanna.tarka@signavio.com>
2021-11-30 17:58:40 +0100

    filter for selected post

--- api-practice/src/components/PostDetails.jsx
+++ api-practice/src/components/PostDetails.jsx
@@ -4,17 +4,24 @@ import { useNavigate } from "react-router-dom";
 
 const PostDetail = ({posts}) => {
     const { id } = useParams()
-    const positionInArray = id-1
+    const selectedPost = (id) => {
+        return posts.filter((post)=>post.id == id)
+    }
     let navigate = useNavigate()
 
     return (
     <div>
         <h2 className='mainTitle'>Post</h2>
         <button onClick={()=>navigate('/')} className='backButton'>Back</button>
-        <div className='post'>
-            <h2 className='postTitle'>{posts[positionInArray].title}</h2>
-            <div>{posts[positionInArray].body}</div>
+        {console.log(selectedPost(id))}
+        {selectedPost(id).map((post)=>{
+            return(
+                <div className='post'>
+                <h2 className='postTitle'>{post.title}</h2>
+                <div>{post.body}</div>
         </div>
+            )
+        })}
     </div>
     )}
 
